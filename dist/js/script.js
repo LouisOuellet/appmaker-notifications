e163e684876620abfc3feb20a4f205b28f13af6f
Louis Ouellet <louis@TheMacSwitch.local>
2021-11-20 15:02:34 -0500

    21.11-dev-2

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,13 @@
 API.Plugins.notifications = {
-	init:function(){},
+	init:function(){
+		var html = '';
+		html += '<li class="nav-item">';
+			html += '<a class="nav-link" data-slide="true" href="#">';
+				html += '<i class="fas fa-bell"></i>';
+			html += '</a>';
+		html += '</li>';
+		$('#navbar-right').prepend(html);
+	},
 }
 
 API.Plugins.notifications.init();
